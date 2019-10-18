66e8f9cee034cce08f573a46a15349746c255f18
Erdi Akyuz <erdiakyuz@gmail.com>
2019-10-18 08:50:00 +0200

    Robot sensor created

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/eak100/workspace/catkin_ws/src
+# Build directory: /home/eak100/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("first_package")
+subdirs("my_robot")
+subdirs("simple_arm")
